ea553de9241847b3331fab92381bb2bf620eea6b
Cohen Adair <cohenadair@gmail.com>
2016-06-19 22:18:05 -0400

    Added demo/testing components

---
+++ app.js
@@ -0,0 +1,82 @@
+(function() {
+     var app = angular.module("number-input-demo", ['number-input']);
+    
+     app.controller("AppController", function() {
+          this.inputsConfig = [
+               [
+                    {
+                         label: "Default",
+                         options: {}
+                    },
+                    {
+                         label: "Custom hint",
+                         options: {
+                              min: 0,
+                              max: 24,
+                              step: 0.25,
+                              hint: "0 to 24 hours",
+                              decimalPlaces: 2
+                         }
+                    }
+               ],
+               [
+                    {
+                         label: "Decimal places",
+                         options: {
+                              start: 0,
+                              min: -1,
+                              max: 1,
+                              step: 0.0005,
+                              hideHint: false,
+                              disableDecimal: false
+                         }
+                    },
+                    {
+                         label: "From -10 to 10",
+                         options: {
+                              start: 0,
+                              min: -10,
+                              max: 10,
+                              hideHint: true
+                         }
+                    }
+               ],
+               [
+                    {
+                         label: "50 or lower",
+                         options: {
+                              start: 45,
+                              max: 50
+                         }
+                    },
+                    {
+                         label: "Always positive",
+                         options: {
+                              start: 0,
+                              min: 0
+                         }
+                    }
+               ],
+               [
+                    {
+                         label: "From 6 to 19",
+                         options: {
+                              start: 6,
+                              min: 6,
+                              max: 19,
+                              hideHint: true
+                         }
+                    },
+                    {
+                         label: "From -6 to -19",
+                         options: {
+                              start: -19,
+                              min: -19,
+                              max: -6,
+                              hideHint: true
+                         }
+                    }
+               ],
+          ];
+     });
+})();
